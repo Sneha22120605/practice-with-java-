6f279b5bf97e4f5e6459459416f015295f5f2cb2
Sneha <155344821+Sneha22120605@users.noreply.github.com>
2024-04-03 14:17:51 +0530

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,9 @@
+public class exercise2 {
+    public static void main(String[] args){
+        //Calculate the sum of 11 and 11
+        int sum=11+11;
+        //Print the result of the addition
+        System.out.println(sum);
+    }
+    
+}
