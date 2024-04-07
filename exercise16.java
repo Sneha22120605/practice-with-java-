8e033ce493566d31dc16f21e6c759ccfbf06c774
Sneha <155344821+Sneha22120605@users.noreply.github.com>
2024-04-07 23:30:12 +0530

    Add files via upload

---
+++ exercise16.java
@@ -0,0 +1,8 @@
+public class exercise16{
+    public static void main(String[] args){
+        System.out.println("   J    a   v       v      a");
+        System.out.println("   J   a  a   v   v     a    a");
+        System.out.println(" J   J a a a    V     a   a  a ");
+        System.out.println("   J  a a a a   v    a a a a a ");
+    }
+}
