5867c3d43aab688115f3cf537a8ef149cb910858
Sneha <155344821+Sneha22120605@users.noreply.github.com>
2024-04-10 22:12:28 +0530

    Add files via upload

---
+++ exercise32.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class exercise32{
+    public static void main(String[] args){
+        int[] my_array1 = { 1789, 4567, 9876, 2004, 2005, 2012, 2022, 1819, 10000, 10000 };
+        String[] my_array2 = { "Java, Python, MYSQL, C Programming, C++, PHP, Python , Javascripy"};
+        System.out.println("Original numeric array :"  + Arrays.toString(my_array1));
+        Arrays.sort(my_array1);
+        System.out.println("Sorted numeric array : " + Arrays.toString(my_array1));
+        System.out.println("Original string array :" + Arrays.toString(my_array2));
+        Arrays.sort(my_array2);
+        System.out.println("Sorted string array :" + Arrays.toString(my_array2));
+    }
+}
+
